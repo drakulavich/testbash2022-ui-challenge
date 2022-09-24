1101337ceed7499180116305e50de1e26d42f882
Anton Yakutovich <drakulavich@gmail.com>
2022-09-24 16:44:52 +0400

    Use contact object

--- tests/mot-challenge.spec.ts
+++ tests/mot-challenge.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
 
+const contact = {
+  name: 'John Snow',
+  email: 'johnny@snow.me',
+  phone: '971581112233',
+  subject: 'Booking request from Winterfell',
+  message: 'Winter is coming! Please accept my request ASAP!\n\nRegards,\nJS',
+};
+
 // https://club.ministryoftesting.com/t/take-the-test-bash-2022-ui-challenge-closing-on-the-25th-of-september-2022/61210
 test('read the message in the admin portal', async ({ page }) => {
   const randomNumber = Math.round(Math.random() * 1000000);
@@ -8,32 +16,30 @@ test('read the message in the admin portal', async ({ page }) => {
 
   await page.locator('button:has-text("let me hack")').click();
 
-  await page.locator('[data-testid="ContactName"]').fill('John Snow');
+  await page.locator('[data-testid="ContactName"]').fill(contact.name);
 
-  await page.locator('[data-testid="ContactEmail"]').fill('johnny@snow.me');
+  await page.locator('[data-testid="ContactEmail"]').fill(contact.email);
 
-  await page.locator('[data-testid="ContactPhone"]').fill('971581112233');
+  await page.locator('[data-testid="ContactPhone"]').fill(contact.phone);
 
-  await page.locator('[data-testid="ContactSubject"]').fill(`Booking request from Winterfell #${randomNumber}`);
+  await page.locator('[data-testid="ContactSubject"]').fill(`${contact.subject} #${randomNumber}`);
 
-  await page.locator('[data-testid="ContactDescription"]').fill('Winter is coming! Please accept my request ASAP!\n\nRegards,\nJS');
+  await page.locator('[data-testid="ContactDescription"]').fill(contact.message);
 
   await page.locator('text=Submit').click();
 
   await page.goto('https://automationintesting.online/#/admin');
 
   await page.locator('[data-testid="username"]').fill('admin');
-
   await page.locator('[data-testid="password"]').fill('password');
-
   await page.locator('[data-testid="submit"]').click();
 
   await page.goto('https://automationintesting.online/#/admin/messages');
 
   await page.locator(`text=${randomNumber}`).click();
-
-  await expect(page.locator('text=johnny@snow.me')).toBeVisible();
-
+  for (const contactValue of Object.values(contact)) {
+    await expect(page.locator(`text=${contactValue}`).first()).toBeVisible();
+  };
   await page.locator('text=Close').click();
 
   // Remove the message
