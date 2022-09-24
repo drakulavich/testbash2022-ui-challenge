fa5b30f69372047f9a538760475804b6d8faa90c
Anton Yakutovich <drakulavich@gmail.com>
2022-09-24 17:54:39 +0400

    Remove example specs

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-
-  // Click [data-testid="DeleteMessage3"]
-  await page.locator('.fa-remove').click();
-
-});
